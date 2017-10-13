b3a543263cb6378adf91265ac5080e863c4187c5
张晓烨 <82387913@qq.com>
2017-10-13 17:29:44 +0800

    zxy 完善小纸条

--- switf-MobileAssistant1/switf-MobileAssistant/Controller/About/Small_piece_paper/small_piece_paperDetailViewController.swift
+++ switf-MobileAssistant1/switf-MobileAssistant/Controller/About/Small_piece_paper/small_piece_paperDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class small_piece_paperDetailViewController: ZXYBaseViewController {
+class small_piece_paperDetailViewController: ZXYBaseViewController,UITextViewDelegate {
 
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -27,51 +27,69 @@ class small_piece_paperDetailViewController: ZXYBaseViewController {
         
         self.contentLabel.text = self.entity.content
         
-//        self.user_name_timeLabel.text = self.entity.create_name! + "     " + (self.entity.create_time?.substring(to: 10))!
-
-//        if (self.entity.img_name.length > 0 ) {
-//            
-//        }else{
-//            self.ImageBtn.alpha = 0;
-//        }
-//        
-//        if ([self.entity.reply_content isEqualToString:@"-1"]) {
-//            
-//            self.reply_contentView.text = @"";
-//            
-//        }else{
-//            self.reply_contentView.text = self.entity.reply_content;
-//        }
-//        
-//        if ([userEntity.type_id intValue] == ROLE_CUSTOMER) {
-//            
-//            if ([self.entity.reply_content isEqualToString:@"-1"]) {
-//                
-//                self.reply_contentView.text = @"";
-//                
-//            }else{
-//                
-//                self.reply_contentView.userInteractionEnabled = NO;
-//                self.reply_contentView.text = self.entity.reply_content;
-//                self.reply_Btn.alpha = 0;
-//            }
-//            
-//            
-//        }else{
-//            
-//            if ([self.entity.reply_content isEqualToString:@"-1"]) {
-//                
-//                self.reply_contentView.alpha = 0;
-//                
-//            }
-//            
-//            self.reply_contentView.userInteractionEnabled = NO;
-//            
-//            self.reply_Btn.alpha = 0;
-//        }
+        self.user_name_timeLabel.text = self.entity.create_name! + "     " + (self.entity.create_time?.substring(to: (self.entity.create_time?.index((self.entity.create_time?.startIndex)!, offsetBy: 10))!))!
 
         
+        if (self.entity.img_name?.characters.count)! > 0 {
+            
+        }else{
+            self.ImageBtn.alpha = 0
+        }
+        
+        if self.entity.reply_content == "-1"{
+            
+            self.reply_contentView.text = ""
+        
+        }else{
+        
+            self.reply_contentView.text = self.entity.reply_content
+        }
+        
+        if myModel.type_id == ROLE_CUSTOMER {
+            
+            if self.entity.reply_content == "-1" {
+                self.reply_contentView.text = ""
+            }else{
+                self.reply_contentView.isUserInteractionEnabled = false
+                self.reply_contentView.text = self.entity.reply_content
+                self.reply_Btn.alpha = 0
+
+            }
+        }else{
+            
+            if self.entity.reply_content == "-1" {
+                
+                self.reply_contentView.alpha = 0
+                
+            }
+            
+            self.reply_contentView.isUserInteractionEnabled = false
+            
+            self.reply_Btn.alpha = 0
+
+        }
+        
     }
+    
+    @IBAction func btnTableViewCellBtnClicked(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func submitBtn(_ sender: UIButton) {
+    
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == "\n"{
+            
+            textView.resignFirstResponder()
+            return false
+        }
+        
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
